c89c65a832e74bc94f6640d6a9d0abeaee069832
wanghao98 <hao_wang@berkeley.edu>
2023-05-01 13:57:50 -0700

    udpate

---
+++ Code/others/pseudobulk_covid143.R
@@ -0,0 +1,37 @@
+library(stringr)
+library(muscat)
+library(Seurat)
+
+myseurat = readRDS("../../data/Processed_Datasets/covid143/covid143_default/covid143_default.Rds")
+
+sce <- as.SingleCellExperiment(myseurat, assay = "raw")
+rm(myseurat)
+sce <- prepSCE(sce, 
+                   kid = "initial_clustering", # subpopulation assignments
+                   gid = "Status",  # group IDs (ctrl/stim)
+                   sid = "patient_id",   # sample IDs (ctrl/stim.1234)
+                   drop = TRUE)
+
+
+pb <- aggregateData(sce, assay = "counts", 
+                           fun = "sum", 
+                           by = c("sample_id"))
+names(pb@assays) = "counts"
+
+counts = pb@assays@data@listData$counts
+seurat_sam <- CreateSeuratObject(counts)
+
+
+seurat_sam  = seurat_sam %>%
+  NormalizeData(verbose = T, normalization.method = "LogNormalize")%>%
+  FindVariableFeatures(selection.method = "vst", nfeatures = 2000)%>%
+  ScaleData(verbose = T)%>%
+  RunPCA(npcs = min(c(ncol(seurat_sam)-1,50)),verbose = T)
+
+plot_df <- cbind(seurat_sam@meta.data, seurat_sam@reductions$pca@cell.embeddings)
+
+
+save(plot_df, file = "../../results/pseudobulk/pseudobulk_covid143.Rda")
+
+
+
---
+++ Code/others/pseudobulk_perez.R
@@ -0,0 +1,28 @@
+library(stringr)
+library(Seurat)
+library(SingleCellExperiment)
+library(muscat)
+
+sce = readRDS("../../data/Processed_Datasets/perez/perez_default/perez_default.Rds")
+sce$sample_id = paste0(sce$sample_uuid,"_", sce$Processing_Cohort)
+
+
+pb <- aggregateData(sce, assay = "counts", 
+                    fun = "sum", 
+                    by = c("sample_id"))
+names(pb@assays) = "counts"
+
+counts = pb@assays@data@listData$counts
+seurat_sam <- CreateSeuratObject(counts)
+
+seurat_sam  = seurat_sam %>%
+  NormalizeData(verbose = T, normalization.method = "LogNormalize")%>%
+  FindVariableFeatures(selection.method = "vst", nfeatures = 2000)%>%
+  ScaleData(verbose = T)%>%
+  RunPCA(npcs = min(c(ncol(seurat_sam)-1,50)),verbose = T)
+
+plot_df <- cbind( seurat_sam@reductions$pca@cell.embeddings)
+
+
+save(plot_df, file = "../../results/pseudobulk/pseudobulk_perez.Rda")
+
